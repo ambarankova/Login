b9870eaaa6efb3cf238a8ac658ced4c7d1ce3917
Александра Баранкова <ambarankova@gmail.com>
2024-07-01 19:27:12 +0200

    HW 2.4

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailBarView: UIView!
+    @IBOutlet weak var passwordBarView: UIView!
+    @IBOutlet weak var newUserLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureButton(loginButton)
     }
 
-
+    // MARK: - Methods
+    
+    private func configureButton(_ button: UIButton) {
+        button.layer.shadowColor = button.backgroundColor?.cgColor
+        button.layer.shadowOffset = CGSize(width: 0, height: 3)
+        button.layer.shadowOpacity = 0.5
+    }
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+        print("Login Button is working")
+    }
+    @IBAction func signUpButtonAction(_ sender: Any) {
+        print("SignUp Button is working")
+    }
+    
 }
 
